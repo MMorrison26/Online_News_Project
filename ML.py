15610cc3d64c0b05b49585a2484992972d9b11fd
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-01-08 12:12:22 -0800

    Update ML.py

--- ML.py
+++ ML.py
@@ -16,18 +16,18 @@ list1 = []
 for i,j in enumerate(application_df):
 	list1.append(j)
 	datafrm1 = pd.DataFrame(list1)
-#new1 = datafrm1.dropna().iloc[0,0]
-#print(new1)
-	ax = sns.heatmap(datafrm1, linewidth=0.5)
-	plt.show()
+new1 = datafrm1.dropna().iloc[0,0]
+print(new1)
+#	ax = sns.heatmap(datafrm1, linewidth=0.5)
+#	plt.show()
 
 #pick the columns with the values that are with treshold high 
-for i, j in enumerate(applications_df):
-        df1 = application_df.iloc[i,i]>1
+#for i, j in enumerate(applications_df):
+#        df1 = application_df.iloc[i,i]>1
 #pick the columns with the values that are with treshold mid
-        df2 = application_df.iloc[i,1]>0.5
+#        df2 = application_df.iloc[i,1]>0.5
 #pick the columsn with the values that are with treshold low 
-        df3 = application_df.iloc[i,1]>0.1
+#        df3 = application_df.iloc[i,1]>0.1
 
 #then perform normalization of the data 
 
